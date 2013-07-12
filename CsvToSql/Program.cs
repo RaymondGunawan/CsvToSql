fac8bc78e5c4eea930b631235fdce2997a1a2b51
rgunawan <raymond.gunawan@software.dell.com>
2013-07-12 14:17:48 -0500

    Added initial code to copy csv to a sql table using sqlbulkcopy

---
+++ CsvToSql/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using LumenWorks.Framework.IO.Csv;
+
+namespace CsvToSql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new SqlConnectionStringBuilder
+                {
+                    DataSource = @"10.6.33.35\sqlexpress",
+                    IntegratedSecurity = false,
+                    UserID = "sa",
+                    Password = "Password1",
+                    InitialCatalog = "Catalog"
+                };
+
+            var stopWatch = new Stopwatch();
+
+            var rowsStopWatch = new Stopwatch();
+
+            stopWatch.Start();
+
+            using (var textReader = File.OpenText(@"C:\Users\rgunawan\Downloads\dataJul-12-2013.csv"))
+            using (var reader = new CsvReader(textReader, true))
+            using (var conn = new SqlConnection(builder.ToString()))
+            using (var sql = new SqlBulkCopy(conn))
+            {
+                conn.Open();
+
+                sql.BatchSize = 10000;
+                
+                sql.NotifyAfter = 10000;
+                sql.SqlRowsCopied +=
+                    (sender, eventArgs) =>
+                    {
+                        Console.WriteLine("Copied: {0} rows in {1} seconds.", eventArgs.RowsCopied, rowsStopWatch.Elapsed.TotalSeconds);
+                        rowsStopWatch.Restart();
+                      
+                    };
+
+                sql.DestinationTableName = "TestTable";
+                rowsStopWatch.Start();
+
+                sql.WriteToServer(reader);
+                
+                sql.Close();
+                conn.Close();
+            }
+
+            stopWatch.Stop();
+
+            Console.WriteLine("Elapsed seconds: " + stopWatch.Elapsed.TotalSeconds);
+            Console.ReadKey();
+        }
+    }
+}
